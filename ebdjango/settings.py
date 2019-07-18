6c2b8590a70534378a8c4a3a9dff65fc1bd7d84f
Mircea-Stefan Mohora <mircea-stefan.mohora@fundingcircle.com>
2019-07-18 10:27:45 +0100

    works in prod

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.pqfngq7tic.us-east-1.elasticbeanstalk.com', '127.0.0.1']
+ALLOWED_HOSTS = ['prod.pqfngq7tic.us-east-1.elasticbeanstalk.com', '127.0.0.1']
 
 
 # Application definition
