9b1037fc4ca01c2579a5635b3c1aff4f04bb274a
Mircea-Stefan Mohora <mircea-stefan.mohora@fundingcircle.com>
2019-09-11 17:30:30 +0100

    Added one small test

--- forecast/tests.py
+++ forecast/tests.py
@@ -1,17 +1,61 @@
 from datetime import timedelta
 import time
+import random
+import string
 
 from django.utils import timezone
 from django.urls import reverse
 from django.test import TestCase
 from django.db.models import Sum
 
-from .models import Form
+from .models import Query, Form, Estimation, SUCCESS_MESSAGE
 
 
 """
-NOTE: METHODS RUN AGAINST A FAKE DB. DB FLUSHES AFTER EACH METHOD.
+NOTE: TEST METHODS RUN AGAINST A FAKE DB. DB FLUSHES AFTER EACH METHOD.
 """
+
+
+def random_string_with_digits_and_symbols(string_length=10):
+    """ Generate a random string of letters, digits and special characters """
+    password_characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(password_characters) for _ in range(string_length))
+
+
+class SimulationTests(TestCase):
+    def test_run_estimation_on_query_with_valid_form(self):
+        """
+        Invalid forms should not create Estimation models and save them db.
+
+        run_estimation should give back corresponding message.
+        """
+
+        query = Query.objects.create(name="query")
+
+        wip_lower_bound = random.randint(0, 10000)
+        wip_upper_bound = random.randint(0, 20000)
+        throughput_lower_bound = random.randint(0, 10000)
+        throughput_upper_bound = random.randint(throughput_lower_bound, 20000)
+        name = random_string_with_digits_and_symbols(100)
+        wip_filter = random_string_with_digits_and_symbols(100)
+        throughput_filter = random_string_with_digits_and_symbols(100)
+        split_factor_lower_bound = random.randint(1, 10000)
+        split_factor_upper_bound = random.randint(split_factor_lower_bound, 20000)
+
+        _ = \
+            Form.objects.create(query=query,
+                                name=name,
+                                wip_lower_bound=wip_lower_bound,
+                                wip_upper_bound=wip_upper_bound,
+                                throughput_lower_bound=throughput_lower_bound,
+                                throughput_upper_bound=throughput_upper_bound,
+                                wip_filter=wip_filter,
+                                throughput_filter=throughput_filter,
+                                split_factor_lower_bound=split_factor_lower_bound,
+                                split_factor_upper_bound=split_factor_upper_bound)
+
+        response = query.run_estimation()
+        self.assertEqual(response, SUCCESS_MESSAGE)
 
 
 # class FormModelTests(TestCase):
