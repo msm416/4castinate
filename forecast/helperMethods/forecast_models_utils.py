bf69cf2ac4658b03c9f4dd010735989d8424f03b
Mircea-Stefan Mohora <mircea-stefan.mohora@fundingcircle.com>
2019-09-13 19:20:06 +0100

    WIP- GRAPHS

--- forecast/helperMethods/forecast_models_utils.py
+++ forecast/helperMethods/forecast_models_utils.py
@@ -1,25 +1,25 @@
 from collections import Counter
+import numpy as np
 
 
 def reduce_durations(durations):
     weeks_to_frequency = sorted(Counter(durations).items())
-
-    weeks = [k for (k, v) in weeks_to_frequency]
+    centile_values = list(map(lambda x: int(x), np.percentile(durations, range(0, 101, 5), interpolation='nearest')))
+    weeks = [(str(k) if k in set(centile_values) else str(k)) for (k, v) in weeks_to_frequency]
+    print(weeks)
     weeks_frequency = [v for (k, v) in weeks_to_frequency]
 
     weeks_frequency_sum = sum(weeks_frequency)
+    centile_values.sort()
+    print(centile_values)
+    print(weeks_frequency)
+    # print(weeks_frequency_sum)
 
     # weeks = [k for (k, v) in weeks_to_frequency][0::int(len(weeks_to_frequency)/20)]
     # weeks_frequency = [v for (k, v) in weeks_to_frequency][0::int(len(weeks_to_frequency)/20)]
 
     # weeks = [k for (k, v) in weeks_to_frequency][0::int(len(weeks_to_frequency)/20)]
     # weeks_frequency = [v for (k, v) in weeks_to_frequency][0::int(len(weeks_to_frequency)/20)]
-
-    durations.sort()
-    centile_indices = []
-    centile_values = []
-    for i in range(0, 21):
-        centile_indices.append(5 * i)
-        centile_values.append(durations[int((weeks_frequency_sum - 1) * 5 * i / 100)])
+    # print(centile_values)
     return centile_values, weeks, weeks_frequency, weeks_frequency_sum
 
